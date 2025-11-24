10a21b3bb794ee3c33352460ec0e9cd94bd24164
Filip Wiśniewski <fwfw541111@gmail.com>
2025-11-24 23:10:33 +0100

    Auth guard and login page styles

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { AppComponent } from './components/app/app.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
-    { path: '', component: AppComponent }
+    { path: '', component: AppComponent, canActivate: [authGuard] }
 ];
