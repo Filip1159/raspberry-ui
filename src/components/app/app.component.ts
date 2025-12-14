0f142d1e7c998aad3ab605463d2029dddb3e2c68
Filip Wiśniewski <fwfw541111@gmail.com>
2025-12-15 00:23:02 +0100

    Remove zone.js

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core'
 import { FormsModule } from '@angular/forms'
-import { AlarmComponent } from './../alarm/alarm.component'
 import { HeaderComponent } from './../header/header.component'
 import { VideoStreamComponent } from './../video-stream/video-stream.component'
 import { SocketService } from './../../socket.service'
@@ -8,7 +7,7 @@ import { SocketService } from './../../socket.service'
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [FormsModule, AlarmComponent, HeaderComponent, VideoStreamComponent],
+    imports: [FormsModule, HeaderComponent, VideoStreamComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
@@ -19,14 +18,12 @@ export class AppComponent implements OnInit {
     inputMessage = ''
 
     ngOnInit() {
-        // Nasłuchiwanie na wiadomości z serwera
         this.socketService.listen('message').subscribe((msg: unknown) => {
             if (typeof msg === 'string') this.message = msg
         })
     }
 
     sendMessage() {
-        // Wysyłanie wiadomości do serwera
         this.socketService.emit('message', this.inputMessage)
     }
 }
